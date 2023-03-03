db3923ca98442322516a6ebbced0de254c82f0ee
James Wilson <james@jsdw.me>
2023-03-03 18:23:29 +0000

    fix copyright headers

--- scale-decode/src/error/context.rs
+++ scale-decode/src/error/context.rs
@@ -1,5 +1,5 @@
 // Copyright (C) 2023 Parity Technologies (UK) Ltd. (admin@parity.io)
-// This file is a part of the scale-value crate.
+// This file is a part of the scale-decode crate.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
