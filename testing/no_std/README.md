e35131290794f7fb9770814e286ad0b011d21349
haerdib <bhaerdi@devsgx02.scs-ad.scs.ch>
2023-06-23 17:30:00 +0200

    add test no std crate

---
+++ testing/no_std/README.md
@@ -0,0 +1,1 @@
+test no_std build of scale-decode and scale-decode-derive
---
+++ testing/no_std/src/lib.rs
@@ -0,0 +1,28 @@
+#![no_std]
+extern crate alloc;
+
+use scale_decode::DecodeAsType;
+
+pub struct NotDecodeAsType;
+
+// Enums with generic params can impl EncodeAsType.
+#[derive(DecodeAsType)]
+pub enum Bar<T, U, V> {
+    Wibble(bool, T, U, V),
+    Wobble,
+}
+
+// This impls EncodeAsType ok; we set no default trait bounds.
+#[derive(DecodeAsType)]
+#[decode_as_type(trait_bounds = "")]
+pub enum NoTraitBounds<T> {
+    Wibble(core::marker::PhantomData<T>),
+}
+
+// Structs (and const bounds) impl EncodeAsType OK.
+#[derive(DecodeAsType)]
+pub struct MyStruct<const V: usize, Bar: Clone + PartialEq> {
+    array: [Bar; V],
+}
+
+pub fn can_decode_as_type<T: DecodeAsType>() {}
