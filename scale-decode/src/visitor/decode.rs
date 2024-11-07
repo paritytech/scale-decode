84ff3bbaf2f12bd31f42ff8afef0a241b2036f91
Niklas Adolfsson <niklasadolfsson1@gmail.com>
2024-11-07 21:47:25 +0100

    Update scale-decode/src/visitor/decode.rs

--- scale-decode/src/visitor/decode.rs
+++ scale-decode/src/visitor/decode.rs
@@ -293,7 +293,7 @@ impl<'temp, 'scale, 'resolver, V: Visitor> ResolvedTypeVisitor<'resolver>
                 } else {
                     u128::decode(data)
                 }
-                .map_err(|e| e.into())?;
+                .map_err(Into::into)?;
                 visitor.visit_u128(n, type_id)
             }
             Primitive::U256 => {
